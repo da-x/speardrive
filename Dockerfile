0312005093678764f1152d0962495c272a6cfe5d
Dan Aloni <dan@kernelim.com>
2022-09-29 13:47:51 +0300

    Extend build.sh for generating docker images

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Build
+FROM rockylinux:8.5.20220308 as builder
+
+RUN echo fastestmirror=True >> /etc/dnf/dnf.conf
+RUN yum -y install epel-release
+RUN yum -y install gcc openssl-devel
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+COPY . /work
+RUN source $HOME/.cargo/env && cd /work && ./build.sh local
+
+# Run-time image
+FROM rockylinux:8.5.20220308
+COPY --from=builder /work/bin/speardrive /dist/speardrive
+CMD ["/dist/speardrive"]
--- build.sh
+++ build.sh
@@ -2,24 +2,46 @@
 
 set -e
 
-EXENAME=speardrive
-dest=bin/${EXENAME}
+local() {
+    EXENAME=speardrive
+    dest=bin/${EXENAME}
 
-while [[ $# != 0 ]] ; do
-    if [[ "$1" == "--dest" ]] ; then
-	shift
-        dest=$1
-        shift
-        continue
-    fi
-    break
-done
+    while [[ $# != 0 ]] ; do
+	if [[ "$1" == "--dest" ]] ; then
+	    shift
+	    dest=$1
+	    shift
+	    continue
+	fi
+	break
+    done
 
-T=/tmp/$USER/rust/targets/`pwd`/target
+    T=/tmp/$USER/rust/targets/$(pwd)/target
 
-mkdir -p $T
-cargo build --release --target-dir ${T}
+    mkdir -p $T
+    cargo build --release --target-dir ${T}
 
-mkdir -p bin/
-rm -f ${dest}
-cp $T/release/${EXENAME} ${dest}
+    mkdir -p bin/
+    rm -f ${dest}
+    cp $T/release/${EXENAME} ${dest}
+}
+
+_docker() {
+    set -eu
+    set -o pipefail
+    set +o posix
+
+    VERSION=$(grep '^version =' Cargo.toml | awk -F'"' '{print $2}')
+    docker build -t alonid/speardrive:${VERSION} .
+    # docker push alonid/speardrive:${VERSION}
+}
+
+if [[ "$1" == "" ]] ; then
+    echo "build.sh docker/local"
+else
+    case $1 in
+	local) "$@" ;;
+	docker) shift; _docker "$@" ;;
+	*) echo invalid command $1; exit -1;;
+    esac
+fi
